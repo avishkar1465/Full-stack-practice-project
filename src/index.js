570aa319ac9b1a3d976efcbb0ee0c893482f9dc8
avishkar_1465 <avishkarparadkar@gmail.com>
2025-06-17 23:37:27 +0530

    routes and controllers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import connectDB from "./db/index.js";
-import 'dotenv/config';
-
-
+import dotenv from "dotenv"
+import {app} from './app.js'
+dotenv.config({
+    path: './.env'
+})
 connectDB()
 .then(() => {
     app.on("error", (error) => {
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,17 +1,27 @@
-// Higher-order function to handle async errors in middleware functions
-const asyncHandler = (fn) => 
-    async (req, res, next) => { // Returns a new async function taking req, res, and next
-        try {
-            // Executes the passed asynchronous function and waits for it to complete
-            await fn(req, res, next);
-        } catch (error) {
-            // Handles any errors that occur during the execution of fn
-            res.status(error.code || 500).json({ // Sends an error response to the client
-                success: false, // Indicates the request was unsuccessful
-                message: error.message // Includes the error message in the response
-            });
-        }
-    };
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
 
-// Exporting the asyncHandler function so it can be used in other files
-export { asyncHandler };
+
+export { asyncHandler }
+
+
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
